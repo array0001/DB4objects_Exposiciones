b627badfcca0ddce5e239bac676a937cc7189f1b
Juan F. Torres <array0001@hotmail.com>
2013-05-17 22:56:00 -0500

    Adding Expositor1 without UI

--- src/expo/Expo.java
+++ src/expo/Expo.java
@@ -18,6 +18,11 @@ public class Expo {
      */
     public static void main(String[] args) {
         ObjectContainer baseDatos = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "exposicion.db4o");
-        
+        Expositor expositor1 = new Expositor("123456","Claudio Valencia","claudio@example.com",100000);
+        try{
+            Metodos.almacenarExpositor(baseDatos, expositor1);
+        }finally{
+            Metodos.cerrarConexion(baseDatos);
+        }
     }
 }
